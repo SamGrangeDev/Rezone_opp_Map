3f08ef24c7838f656be49249440175b972334c3e
Sam Josh <sam.samudio@grangedevelopment.com>
2024-09-20 08:20:31 +0800

    update
    add google maps

--- layers/layers.js
+++ layers/layers.js
@@ -3,7 +3,19 @@ ol.proj.proj4.register(proj4);
 var wms_layers = [];
 
 
-        var lyr_ESRISatelliteArcGISWorld_Imagery_0 = new ol.layer.Tile({
+        var lyr_GoogleMaps_0 = new ol.layer.Tile({
+            'title': 'Google Maps',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'
+            })
+        });
+
+        var lyr_ESRISatelliteArcGISWorld_Imagery_1 = new ol.layer.Tile({
             'title': 'ESRI Satellite (ArcGIS/World_Imagery)',
             //'type': 'base',
             'opacity': 1.000000,
@@ -14,34 +26,34 @@ var wms_layers = [];
                 url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
             })
         });
-var format_LGA_1 = new ol.format.GeoJSON();
-var features_LGA_1 = format_LGA_1.readFeatures(json_LGA_1, 
+var format_LGA_2 = new ol.format.GeoJSON();
+var features_LGA_2 = format_LGA_2.readFeatures(json_LGA_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:4283'});
-var jsonSource_LGA_1 = new ol.source.Vector({
+var jsonSource_LGA_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_LGA_1.addFeatures(features_LGA_1);
-var lyr_LGA_1 = new ol.layer.Vector({
+jsonSource_LGA_2.addFeatures(features_LGA_2);
+var lyr_LGA_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_LGA_1, 
-                style: style_LGA_1,
+                source:jsonSource_LGA_2, 
+                style: style_LGA_2,
                 popuplayertitle: "LGA",
                 interactive: true,
     title: 'LGA<br />\
-    <img src="styles/legend/LGA_1_0.png" /> Currently Monitoring<br />\
-    <img src="styles/legend/LGA_1_1.png" /> Not yet monitored<br />\
-    <img src="styles/legend/LGA_1_2.png" /> To be monitored<br />'
+    <img src="styles/legend/LGA_2_0.png" /> Currently Monitoring<br />\
+    <img src="styles/legend/LGA_2_1.png" /> Not yet monitored<br />\
+    <img src="styles/legend/LGA_2_2.png" /> To be monitored<br />'
         });
 var group_Basemap = new ol.layer.Group({
-                                layers: [lyr_ESRISatelliteArcGISWorld_Imagery_0,],
+                                layers: [lyr_GoogleMaps_0,lyr_ESRISatelliteArcGISWorld_Imagery_1,],
                                 fold: "open",
                                 title: "Basemap"});
 
-lyr_ESRISatelliteArcGISWorld_Imagery_0.setVisible(true);lyr_LGA_1.setVisible(true);
-var layersList = [group_Basemap,lyr_LGA_1];
-lyr_LGA_1.set('fieldAliases', {'fid': 'fid', 'LGA_NAME24': 'LGA_NAME24', 'STE_NAME21': 'STE_NAME21', 'Status': 'Status', 'GF/Infill': 'GF/Infill', });
-lyr_LGA_1.set('fieldImages', {'fid': 'TextEdit', 'LGA_NAME24': 'TextEdit', 'STE_NAME21': 'TextEdit', 'Status': 'TextEdit', 'GF/Infill': 'Range', });
-lyr_LGA_1.set('fieldLabels', {'fid': 'hidden field', 'LGA_NAME24': 'inline label - visible with data', 'STE_NAME21': 'inline label - visible with data', 'Status': 'inline label - visible with data', 'GF/Infill': 'inline label - visible with data', });
-lyr_LGA_1.on('precompose', function(evt) {
+lyr_GoogleMaps_0.setVisible(false);lyr_ESRISatelliteArcGISWorld_Imagery_1.setVisible(true);lyr_LGA_2.setVisible(true);
+var layersList = [group_Basemap,lyr_LGA_2];
+lyr_LGA_2.set('fieldAliases', {'fid': 'fid', 'LGA_NAME24': 'LGA_NAME24', 'STE_NAME21': 'STE_NAME21', 'Status': 'Status', 'GF/Infill': 'GF/Infill', });
+lyr_LGA_2.set('fieldImages', {'fid': 'TextEdit', 'LGA_NAME24': 'TextEdit', 'STE_NAME21': 'TextEdit', 'Status': 'TextEdit', 'GF/Infill': 'Range', });
+lyr_LGA_2.set('fieldLabels', {'fid': 'hidden field', 'LGA_NAME24': 'inline label - visible with data', 'STE_NAME21': 'inline label - visible with data', 'Status': 'inline label - visible with data', 'GF/Infill': 'inline label - visible with data', });
+lyr_LGA_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
